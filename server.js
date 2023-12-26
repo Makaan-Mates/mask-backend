bc81fd91cdc7d9e45f9a972022235904ac33ef7b
Sourabh <rathoursourabh5@gmail.com>
2023-12-26 16:50:55 +0530

    fixed bugs related to jwt tokens

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const jwt = require('jsonwebtoken')
-const secretKeyForAuthentication = 'Mask@_#1045718'
-const mongoose = require('mongoose')
-app.use(express.json())
+const express = require("express");
+const app = express();
+const jwt = require("jsonwebtoken");
+const secretKeyForAuthentication = "Mask@_#1045718";
+const mongoose = require("mongoose");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 // Connecting to mongodb...
-mongoose.connect('mongodb+srv://rathoursourabh45:Mask%40%5F%231045718@cluster0.px2olez.mongodb.net/users')
+mongoose.connect(
+  "mongodb+srv://rathoursourabh45:Mask%40%5F%231045718@cluster0.px2olez.mongodb.net/users"
+);
 
 // Copied from stack overflow - checking the connection to the database
 const db = mongoose.connection;
@@ -15,88 +19,94 @@ db.once("once", (error, response) => {
 });
 
 // Schema banane ke prakriya
-const User = mongoose.model('User',{
-    email: {
-        type: String,
-        required: true,
-        unique: true
-    },
-    username: String,
-    password:String
+const User = mongoose.model("User", {
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  username: {
+    type:String,
+    unique: true
+  },
+  password: {
+    type:String,
+    required:true
+  },
+});
 
-})
+const verifyToken = (req, res, next) => {
+  const authHeaders = req.headers.authorization;
+  const token = authHeaders && authHeaders.split(" ")[1];
 
-const verifyToken = (req,res,next)=>{
-const authHeaders = req.headers.authorization;
-const token = authHeaders && authHeaders.split(" ")[1]
+  if (token === null) return res.status(401).json({message:'token not found'});
 
-if(token===null) return res.status(404).send('Not authorized')
-
-jwt.verify(token,secretKeyForAuthentication,(err,decoded)=>{
-    if(err){
-        res.status(404).send("Not authorized")
+  jwt.verify(token, secretKeyForAuthentication, (err, decoded) => {
+    if (err) {
+      res.status(401).json({
+        message:"invalid token"
+      });
     } else {
-        res.send("authorized")
-        req.user = decoded
-        next()
+      res.json({
+        message:"authorized"
+      });
+      req.user = decoded;
+      next();
     }
-})
-}
+  });
+};
 
-app.get('/api/home' , verifyToken, (req,res)=>{
-    console.log(req.user)
-    res.json({
-        key: "hello"
-    })
-})
+app.get("/api/home", verifyToken, (req, res) => {
+  console.log(req.user);
+  res.json({
+    key: "hello",
+  });
+});
 
-app.post('/register', async (req,res)=>{
-    const email = req.body.email;
-    const password = req.body.password;
-    const username = req.body.username;
+app.post("/register", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const username = req.body.username;
 
-    const newUser = await User({
-        email:email,
-        username:username,
-        password:password
-    })  
+  const newUser = await User({
+    email: email,
+    username: username,
+    password: password,
+  });
 
-    const savedUser = await newUser.save()
+  const savedUser = await newUser.save();
 
-    res.status(201).send("account created")
+  res.status(201).json({
+    message: "account created!",
+  });
+});
 
-})
-
-app.post('/signin', async (req,res)=>{
+app.post("/login", async (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  const user = await User.findOne({email:email})
+  const user = await User.findOne({ email: email });
 
-  if(user===null){
-    res.status(404).send("account not found")
-    return
+  if (user === null) {
+    res.status(404).json({ message: "account not found" });
+    return;
   }
 
   try {
-    if(await user.password===password){
-        const token = jwt.sign({email:email}, secretKeyForAuthentication)
-        res.status(200).json({
-            token:token
-        })
-
+    if (await user.password === password) {
+      const token = jwt.sign({ email: email }, secretKeyForAuthentication);
+      res.status(200).json({
+        token: token,
+        message:"logged in"
+      });
+    } else {
+      res.status(401).json({ message: "wrong password bro" });
     }
-    else {
-        res.send("wrong password bro")
-    }
-
   } catch {
-    res.status(500).send()
+    res.status(500).send();
   }
+});
 
-
-})
-
-app.listen(4000 ,()=>{
-    console.log("server started...")
-})
+app.listen(4000, () => {
+  console.log("server started...");
+});
