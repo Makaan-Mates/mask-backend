26bc659d7855cd83a5a4b128f4e370b0313adf8a
shadevkumar <shadevsrk261@gmail.com>
2024-01-04 00:44:18 +0530

    Timestamp logic added in FetchUserPosts

--- src/api/controllers/users/profile.js
+++ src/api/controllers/users/profile.js
@@ -1,18 +1,25 @@
-const Post = require("../../models/post.model")
+const Post = require('../../models/post.model')
+const timeSinceCreated = require('../../helpers/timestamp-algo')
+const { formatTimeSince } = timeSinceCreated
 
 const userPostsInfo = async (req, res) => {
-    try {
-      const userPosts = await Post.find({ user_id: req.params.userId })
-        .populate("user_id")
-        .exec();
-  
-      res.json(userPosts);
-    } catch (error) {
-      console.log(error);
-      res.status(500).json({ message: "Internal Server error" });
-    }
+  try {
+    const userPosts = await Post.find({ user_id: req.params.userId })
+      .populate('user_id')
+      .exec()
+
+    const userPostsWithTime = userPosts.map((post) => ({
+      ...post._doc,
+      timeSinceCreated: formatTimeSince(new Date(post.createdAt)),
+    }))
+
+    res.json(userPostsWithTime)
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({ message: 'Internal Server error' })
   }
+}
 
 module.exports = {
-    userPostsInfo
+  userPostsInfo,
 }
