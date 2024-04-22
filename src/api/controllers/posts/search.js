a3b65bc79f096b68d4e5bb83f265944bb1343775
shadevkumar <shadevsrk261@gmail.com>
2024-04-22 17:12:52 +0530

    Search querying optimised, used mongoDB aggregation pipleline

--- src/api/controllers/posts/search.js
+++ src/api/controllers/posts/search.js
@@ -1,32 +1,60 @@
 const Post = require('../../models/post.model')
+const User = require('../../models/user.model') // Import the User model
 const timeSinceCreated = require('../../helpers/timestamp-algo')
 const { formatTimeSince } = timeSinceCreated
 
 const searchPosts = async (req, res) => {
   try {
     const searchQuery = new RegExp(`${req.params.searchQuery}`, 'i')
-    const searchResults = await Post.find({
-      $or: [
-        { title: searchQuery },
-        { description: searchQuery },
-      ],
-    })
-      .populate({
-        path: 'user_id',
-        select: 'username college profession bio  -_id',
-      })
-      .exec()
 
-    const postsearchResults = searchResults.map((post) => ({
-      ...post._doc,
+    // Using aggregation to optimize the search
+    const searchResults = await Post.aggregate([
+      {
+        $lookup: {
+          from: 'users',
+          localField: 'user_id',
+          foreignField: '_id',
+          as: 'userDetails',
+        },
+      },
+      {
+        $match: {
+          $or: [
+            { title: searchQuery },
+            { description: searchQuery },
+            { 'userDetails.college': searchQuery },
+          ],
+        },
+      },
+      {
+        $unwind: '$userDetails', // Deconstructs the userDetails array
+      },
+      {
+        $project: {
+          title: 1,
+          description: 1,
+          createdAt: 1,
+          'userDetails.username': 1,
+          'userDetails.college': 1,
+          'userDetails.profession': 1,
+          'userDetails.bio': 1,
+          totalUpvotes: { $size: '$upvotes' },
+        },
+      },
+    ])
+
+    // Format the search results to include time since created
+    const formattedResults = searchResults.map((post) => ({
+      ...post,
       timeSinceCreated: formatTimeSince(new Date(post.createdAt)),
     }))
 
     res.json({
-      message: postsearchResults,
+      message: formattedResults,
     })
   } catch (error) {
     console.log(error)
+    res.status(500).send('Server error')
   }
 }
 
